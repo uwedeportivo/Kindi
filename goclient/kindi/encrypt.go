bf36c6450d6fda6cbeb77393ed09cea688532a86
uwedeportivo <uwe@codemanic.com>
2011-05-20 07:01:44 -0700

    initial commit:

---
+++ goclient/kindi/encrypt.go
@@ -0,0 +1,204 @@
+package kindi
+
+import (
+        "bytes"
+        "crypto"
+        "crypto/aes"
+        "crypto/cipher"
+        "crypto/rand"
+        "crypto/rsa"
+        "crypto/sha1"
+        "encoding/binary"
+        "fmt"
+        "io"
+        "os"
+)
+
+type Envelope struct {
+        SenderEmail []byte
+        SenderKey *rsa.PrivateKey
+        RecipientKey *rsa.PublicKey
+}
+
+type KeychainFunc func(email []byte) (*rsa.PublicKey, os.Error)
+
+func newCipherStream(symmetricKey []byte) (cipher.Stream, os.Error) {
+        c, err := aes.NewCipher(symmetricKey)
+        if err != nil {
+                return nil, err
+        }
+
+        if c == nil {
+                return nil, fmt.Errorf("Failed to create cipher")
+        }
+
+        iv := make([]byte, c.BlockSize())
+
+        stream := cipher.NewOFB(c, iv)
+        
+        if stream == nil {
+                return nil, fmt.Errorf("Failed to create cipher.Stream")
+        }
+        return stream, nil
+}
+
+func writeLengthEncoded(w io.Writer, data []byte) os.Error {
+        err := binary.Write(w, binary.BigEndian, int64(len(data)))
+        if err != nil {
+                return err
+        }
+        _, err = w.Write(data)
+        if err != nil {
+                return err
+        }
+        return nil
+}
+
+func readLengthEncoded(r io.Reader) (data []byte, err os.Error) {
+        var dataLen int64
+        err = binary.Read(r, binary.BigEndian, &dataLen)
+        if err != nil {
+                return nil, err
+        }
+        data = make([]byte, dataLen)
+        _, err = r.Read(data)
+        if err != nil {
+                return nil, err
+        }
+        return data, nil
+}
+
+func (envelope *Envelope) NewHeader(symmetricKey []byte) (header []byte, err os.Error) {
+        result := bytes.NewBuffer(make([]byte, 0, 1024))
+
+        hash := sha1.New()
+        encryptedSymmetricKey, err := rsa.EncryptOAEP(hash, rand.Reader, envelope.RecipientKey, symmetricKey, nil)
+        if err != nil {
+                return nil, err
+        }
+
+        err = writeLengthEncoded(result, encryptedSymmetricKey)
+        if err != nil {
+                return nil, err
+        }
+
+        buf := bytes.NewBuffer(make([]byte, 0, 1024))
+
+        err = writeLengthEncoded(buf, envelope.SenderEmail)
+        if err != nil {
+                return nil, err
+        }
+
+        hash = sha1.New()
+        hash.Write(envelope.SenderEmail)
+        sum := hash.Sum()
+        sig, err := rsa.SignPKCS1v15(rand.Reader, envelope.SenderKey, crypto.SHA1, sum)
+        if err != nil {
+                return nil, err
+        }
+
+        err = writeLengthEncoded(buf, sig)
+        if err != nil {
+                return nil, err
+        }
+
+        stream, err := newCipherStream(symmetricKey)
+        if err != nil {
+                return nil, err
+        }
+
+        encryptWriter := &cipher.StreamWriter{S: stream, W: result}
+        io.Copy(encryptWriter, buf)
+
+        return result.Bytes(), nil
+}
+
+func DecryptHeader(header []byte, priv *rsa.PrivateKey, keychain KeychainFunc) ([]byte, os.Error) {
+        buf := bytes.NewBuffer(header)
+
+        encryptedSymmetricKey, err := readLengthEncoded(buf)
+        if err != nil {
+                return nil, err
+        }
+
+        hash := sha1.New()
+        decrypted, err := rsa.DecryptOAEP(hash, rand.Reader, priv, encryptedSymmetricKey, nil)
+        if err != nil {
+                return nil, err
+        }
+
+        stream, err := newCipherStream(decrypted)
+        if err != nil {
+                return nil, err
+        }
+
+        tempBuf := bytes.NewBuffer(make([]byte, 0, 1024))
+        decryptReader := &cipher.StreamReader{S: stream, R: buf}
+
+        io.Copy(tempBuf, decryptReader)
+ 
+        senderEmail, err := readLengthEncoded(tempBuf)
+        if err != nil {
+                return nil, err
+        }
+
+        sig, err := readLengthEncoded(tempBuf)
+        if err != nil {
+                return nil, err
+        }
+
+	sender, err := keychain(senderEmail)
+
+        hash = sha1.New()
+        hash.Write(senderEmail)
+        sum := hash.Sum()
+        err = rsa.VerifyPKCS1v15(sender, crypto.SHA1, sum, sig)
+        if err != nil {
+                return nil, err
+        }
+
+        return decrypted, nil
+}
+
+func (envelope *Envelope) Encrypt(w io.Writer, r io.Reader, symmetricKey []byte) os.Error {
+        header, err := envelope.NewHeader(symmetricKey)
+        if err != nil {
+                return err
+        }
+        
+        err = writeLengthEncoded(w, header)
+        if err != nil {
+                return err
+        }
+
+        stream, err := newCipherStream(symmetricKey)
+        if err != nil {
+                return err
+        }
+
+        encryptWriter := &cipher.StreamWriter{S: stream, W: w}
+        io.Copy(encryptWriter, r)
+        return nil
+}
+
+func Decrypt(w io.Writer, r io.Reader, priv *rsa.PrivateKey, keychain KeychainFunc) os.Error {
+        header, err := readLengthEncoded(r)
+        if err != nil {
+                return err
+        }
+
+        symmetricKey, err := DecryptHeader(header, priv, keychain)
+        if err != nil {
+                return err
+        }
+        
+        stream, err := newCipherStream(symmetricKey)
+        if err != nil {
+                return err
+        }
+
+        decryptReader := &cipher.StreamReader{S: stream, R: r}
+        io.Copy(w, decryptReader)
+        return nil
+}
+  
---
+++ goclient/kindi/encrypt_test.go
@@ -0,0 +1,84 @@
+package kindi
+
+import (
+        "bytes"
+        "crypto/rand"
+        "crypto/rsa"
+	"os"
+        "testing"
+)
+
+func newEnvelope(t *testing.T) (*Envelope, *rsa.PublicKey, *rsa.PrivateKey)  {
+        size := 1024
+        sender, err := rsa.GenerateKey(rand.Reader, size)
+        if err != nil {
+		t.Fatalf("failed to generate sender key")
+	}
+        recipient, err := rsa.GenerateKey(rand.Reader, size)
+	if err != nil {
+		t.Fatalf("failed to generate recipient key")
+	}
+
+        return &Envelope{
+        SenderEmail:[]byte("foo@gmail.com"),
+        SenderKey:sender,
+        RecipientKey:&recipient.PublicKey,
+        }, &sender.PublicKey, recipient
+}
+
+func TestNewHeader(t *testing.T) {
+        envelope, sender, recipient := newEnvelope(t)
+
+        symmetricKey := make([]byte, 32)
+        rand.Read(symmetricKey)
+
+        header, err := envelope.NewHeader(symmetricKey)
+        if err != nil {
+		t.Fatalf("failed new header %v", err)
+	}
+        
+        decryptedKey, err := DecryptHeader(header, recipient, func(email []byte) (*rsa.PublicKey, os.Error) {
+		return sender, nil
+	})
+        if err != nil {
+		t.Fatalf("failed decrypt header %v", err)
+	}
+        if ! bytes.Equal(symmetricKey, decryptedKey) {
+                t.Fatalf("expected symmetric key and decrypted symmetric key not equal")
+        }
+}
+
+func TestEncrypt(t *testing.T) {
+        payload := []byte{0xd9, 0x4a, 0xe0, 0x83, 0x2e, 0x64, 0x45, 0xce,
+		0x42, 0x33, 0x1c, 0xb0, 0x6d, 0x53, 0x1a, 0x82, 0xb1,
+		0xdb, 0x4b, 0xaa, 0xd3, 0x0f, 0x74, 0x6d, 0xc9, 0x16,
+		0xdf, 0x24, 0xd4, 0xe3, 0xc2, 0x45, 0x1f, 0xff, 0x59,
+		0xa6, 0x42, 0x3e, 0xb0, 0xe1, 0xd0, 0x2d, 0x4f, 0xe6,
+		0x46, 0xcf, 0x69, 0x9d, 0xfd, 0x81, 0x8c, 0x6e, 0x97,
+		0xb0, 0x51}
+        inbuffer := bytes.NewBuffer(payload)
+        outbuffer := bytes.NewBuffer(make([]byte, 0, 1024))
+
+        envelope, sender, recipient := newEnvelope(t)
+
+        symmetricKey := make([]byte, 32)
+        rand.Read(symmetricKey)
+
+        err := envelope.Encrypt(outbuffer, inbuffer, symmetricKey)
+        if err != nil {
+		t.Fatalf("failed to encrypt %v", err)
+	}
+
+        roundtripbuffer := bytes.NewBuffer(make([]byte, 0, 1024))
+        
+        err = Decrypt(roundtripbuffer, outbuffer, recipient, func(email []byte) (*rsa.PublicKey, os.Error) {
+		return sender, nil
+	})
+        if err != nil {
+		t.Fatalf("failed to decrypt %v", err)
+	}
+        
+        if ! bytes.Equal(roundtripbuffer.Bytes(), payload) {
+                t.Fatalf("decrypted payload different from original payload")
+        }
+}
