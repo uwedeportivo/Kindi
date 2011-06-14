b4ac0ed50f68572985aa3c5f268200c77bbeb555
uwedeportivo <uwe@codemanic.com>
2011-06-13 22:32:55 -0700

    ability to add payload to arbitrary image (in LSB of each color component)

--- goclient/kindi/bitmap.go
+++ goclient/kindi/bitmap.go
@@ -153,3 +153,108 @@ func DecodePNG(rin io.Reader) ([]byte, os.Error) {
 	return rv, nil
 }
 
+type nrgbaImageLSBReaderWriter struct {
+	m *image.NRGBA
+	x, y, q int
+}
+
+func newNRGBAImageLSBReaderWriter(im image.Image) *nrgbaImageLSBReaderWriter {
+	rv := new(nrgbaImageLSBReaderWriter)
+
+	rv.x = 0
+	rv.y = 0
+	rv.q = -1
+
+	b := im.Bounds()
+
+	rv.m = image.NewNRGBA(b.Max.X - b.Min.X, b.Max.Y - b.Min.Y)
+
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			rv.m.Set(x - b.Min.X, y - b.Min.Y, im.At(x, y))
+		}
+	}
+	return rv
+}
+
+func (it *nrgbaImageLSBReaderWriter) reset() {
+	it.x = 0
+	it.y = 0
+	it.q = -1
+}
+
+func (it *nrgbaImageLSBReaderWriter) Read(p []byte) (n int, err os.Error) {
+	n = 0
+	for j, _ := range p {
+		var rv byte = 0
+		var i uint8
+		for  i = 0; i < 8; i++ {
+			it.q++
+			if it.q == 3 {
+				it.q = 0
+				it.x++
+				if it.x == it.m.Rect.Max.X {
+					it.x = it.m.Rect.Min.X
+					it.y++
+					if it.y == it.m.Rect.Max.Y {
+						return n, os.EOF
+					}
+				}
+			}
+			
+			color := it.m.At(it.x, it.y).(image.NRGBAColor)
+			var colorByte byte
+			switch (it.q) {
+			case 0 : colorByte = color.R
+			case 1 : colorByte = color.G
+			case 2 : colorByte = color.B
+			}
+			rv = rv | ((colorByte & 1) << i)
+		}
+		p[j] = rv
+		n++
+	}
+	return n, nil
+}
+
+func setLSB(val, bit byte) byte {
+	var rv byte
+	if bit == 1 {
+		rv = val | 1
+	} else {
+		rv = val & 0xfe
+	}
+	return rv
+}
+
+func (it *nrgbaImageLSBReaderWriter) Write(p []byte) (n int, err os.Error) {
+	n = 0
+	for _, v := range p {
+		var i uint8
+		for i = 0; i < 8; i++ {
+			it.q++
+			if it.q == 3 {
+				it.q = 0
+				it.x++
+				if it.x == it.m.Rect.Max.X {
+					it.x = it.m.Rect.Min.X
+					it.y++
+					if it.y == it.m.Rect.Max.Y {
+						return n, os.EOF
+					}
+				}
+                       }
+			
+			color := it.m.At(it.x, it.y).(image.NRGBAColor)
+			switch (it.q) {
+			case 0 : color.R = setLSB(color.R, (v >> i) & 1)
+			case 1 : color.G = setLSB(color.G, (v >> i) & 1)
+			case 2 : color.B = setLSB(color.B, (v >> i) & 1)
+			}
+			it.m.Set(it.x, it.y, color)
+		}
+		n++
+	}
+	return n, nil
+}
+
